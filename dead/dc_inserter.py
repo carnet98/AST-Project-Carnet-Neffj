dcd6717c29678e71f6201416461e4521d1c3dd7a
carnet98 <carnet@student.ethz.ch>
2022-04-06 08:14:57 +0000

    quick save

--- dead/dc_inserter.py
+++ dead/dc_inserter.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python3
 
+def run():
+    print("RUN THE DC INSERTER")
 
 
+
+
+
+
+# enter program from generator.py
 def entrance(candidate_code):
     # store program in tmp/
     program = open('tmp/candidate.c', 'w+')
     program.write(candidate_code)
     program.close()
+    run()
+
+# enter program standalone
+def main():
+    run()
+
+if __name__ == "__main__":
+    main()
 
     
 
