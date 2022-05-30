9738bf668e7fa1849d3e5aa13486eb30e1892ca9
carnet98 <carnet@student.ethz.ch>
2022-05-30 21:06:08 +0000

    monday case save

--- dead/gdb_script.sh
+++ dead/gdb_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ./tmp
 gcc candidate.c -I/usr/include/csmith-2.3.0/ -g -o candidate
-timeout 60s gdb -batch -x command_file.txt candidate
+timeout 30s gdb -batch -x command_file.txt candidate
 cd ..
